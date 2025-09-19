40ddce8baefea82c35f50819693a8ec496acfb5f
Gaspachoo <gaspard.lefort@outlook.fr>
2025-09-19 15:43:38 +0200

    edit game

--- src/main.js
+++ src/main.js
@@ -10,23 +10,26 @@ function changeColor() {
 let visibleCards = [];
 
 function changeCardState(card) {
-  const compt = visibleCards.length;
-  if (compt == 2) {
-    let cardA = visibleCards[0];
-    let cardB = visibleCards[1]
-    if (cardA.innerHTML == cardB.innerHTML) {
-      console.log("This is a pair !!");
-      cardA.style.visibility = 'hidden';
-      cardB.style.visibility = 'hidden';
-    }
-  }
 
-  if (compt >= 2) {
-    visibleCards.forEach((c) => {
-      c.style.contentVisibility = "hidden";
-    });
-    visibleCards = [];
-  }
   visibleCards.push(card);
   card.style.contentVisibility = "visible";
+  const compt = visibleCards.length;
+
+  if (compt == 2) {
+    let cardA = visibleCards[0];
+    let cardB = visibleCards[1];
+    visibleCards = [];
+    if (cardA.innerHTML == cardB.innerHTML) {
+      console.log("This is a pair !!");
+      setTimeout(() => {
+        cardA.style.visibility = 'hidden';
+        cardB.style.visibility = 'hidden';
+      }, 500);
+    } else {
+      setTimeout(() => {
+        cardA.style.contentVisibility = 'hidden';
+        cardB.style.contentVisibility = 'hidden';
+      }, 1500);
+    }
+  }
 }
