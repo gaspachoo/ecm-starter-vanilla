40df8d10cc7d5ed50a37b7bf26a6e58dd391f45a
Gaspachoo <gaspard.lefort@outlook.fr>
2025-09-19 14:55:12 +0200

    make card visible when clicked (2 max)

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 // Ici vous pouvez écrire votre javascript
 
+let compt = 0;
+
 function changeColor() {
   const title = document.querySelector('h1');
   if (!title) {
     console.error('Title is not defined');
   }
-
-  const randomHexaColor =
-    '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0');
-
+  const randomHexaColor = '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0');
   title.style.color = randomHexaColor;
+}
+
+function changeCardColor(card) {
+
+  if (compt >= 2) {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((c) => {
+      c.style.contentVisibility = "hidden";
+    });
+    compt = 0;
+  }
+  compt += 1;
+  card.style.contentVisibility = "visible";
 }
