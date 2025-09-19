0e3e4360338e53720e0b28d721fc4f87c326604a
Gaspachoo <gaspard.lefort@outlook.fr>
2025-09-19 16:17:05 +0200

    add confettis

--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
-function changeColor() {
-  const title = document.querySelector('h1');
-  if (!title) {
-    console.error('Title is not defined');
-  }
-  const randomHexaColor = '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0');
-  title.style.color = randomHexaColor;
-}
+import JSConfetti from 'js-confetti'
 
 let visibleCards = [];
 
@@ -15,10 +8,10 @@ function changeCardState(card) {
   card.style.contentVisibility = "visible";
   const compt = visibleCards.length;
 
-  if (compt == 2) {
+  if (compt === 2) {
     let cardA = visibleCards[0];
     let cardB = visibleCards[1];
-    if (cardA.innerHTML == cardB.innerHTML) {
+    if (cardA.innerHTML === cardB.innerHTML) {
       console.log("This is a pair !!");
       setTimeout(() => {
         cardA.style.visibility = 'hidden';
@@ -27,10 +20,37 @@ function changeCardState(card) {
       visibleCards = [];
     }
   }
-  if (compt >= 3) {
+  if (compt === 3) {
     visibleCards[0].style.contentVisibility = 'hidden';
     visibleCards[1].style.contentVisibility = 'hidden';
     visibleCards = [visibleCards[2]];
-    console.log(visibleCards);
+
   }
+  if (compt > 3) {
+    console.error('Problème, 3 cartes sont visibles!');
+  }
+
+  const remainingCards = getRemainingCards();
+  console.log(remainingCards);
+  if (remainingCards.length === 0) {
+    const jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti();
+  }
+
+}
+
+window.changeCardState = changeCardState;
+window.getRemainingCards = getRemainingCards;
+
+function getRemainingCards() {
+  const allCards = document.querySelectorAll('.card');
+  const remaining = [];
+
+  allCards.forEach(card => {
+    if (card.style.visibility !== 'hidden') {
+      remaining.push(card);
+    }
+  });
+
+  return remaining;
 }
